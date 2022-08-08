460c7b1142eff6b327ae8c3e824ced778ae8b368
Gabriel <gopxfs@hotmail.com>
2022-08-08 18:58:05 -0300

    Add test cases

--- data.sql
+++ data.sql
@@ -1,5 +1,33 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (
+1,
+'Agumon',
+'Feb 3, 2020',
+0,
+'1',
+20.23);
+
+INSERT INTO animals VALUES (
+2,
+'Gabumon',
+'Nov 15, 2018',
+2,
+'1',
+8);
+
+INSERT INTO animals VALUES (
+3,
+'Pikachu',
+'Jan 7, 2021',
+1,
+'0',
+15.04);
+
+INSERT INTO animals VALUES (
+4,
+'Devimon',
+'May 12, 2017',
+5,
+'1',
+11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from animals where name like '%mon%';
+select name from animals where date_of_birth > '2016-12-31' and date_of_birth < '2019-01-01';
+select name from animals where neutered = '1' and escape_attempts < 3;
+select date_of_birth from animals where name = 'Pikachu' or name = 'Agumon';
+select name, escape_attempts from animals where weight_kg > 10.5;
+select * from animals where neutered = '1';
+select * from animals where name != 'Gabumon';
+select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
